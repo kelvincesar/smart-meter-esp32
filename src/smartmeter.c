e0a21d5328fc70b8008bc89c9fe3c544809ac99c
Kelvin César de Andrade <kelvincandrade@gmail.com>
2020-10-08 23:27:27 -0300

    Correção na leitura do ADC usando o drive i2s- Adicionado freeRTOS e separado em core as funções atuais - Criado função para gerar sinal senoidal 60 Hz para testes

--- src/smartmeter.c
+++ src/smartmeter.c
@@ -12,8 +12,8 @@
 // # ADC configuration:
 #define I2S_NUM 0                       // I2S drive number;
 #define ADC_NUM_OF_CH 2                 // Number of channels that are read;
-#define ADC_SAMPLING_RATE   30000       // Sampling rate in Hz
-#define ADC_BUFFER_SIZE    (ADC_SAMPLING_RATE*ADC_NUM_OF_CH)/MAIN_FREQ
+#define ADC_SAMPLING_RATE   60000       // Sampling rate in Hz
+#define ADC_BUFFER_SIZE     1000        //(ADC_SAMPLING_RATE*ADC_NUM_OF_CH)/MAIN_FREQ
 
 #define ADC_DMA_COUNT       8           // Number of DMA buffers
 
@@ -23,19 +23,21 @@
 #define ADC_SIGNAL_IS_AC    true        // Define that signal read is AC;
 #define ADC_SIGNAL_OFFSET   2048        // Define offset for AC signal;
 
+#define SIN_WAVE_NUM        255         // 112 samples  
 
 static void adc_i2s_init(void)
 {
     static const i2s_config_t i2s_config = {
         .mode = I2S_MODE_MASTER | I2S_MODE_RX | I2S_MODE_ADC_BUILT_IN,
-        .sample_rate = ADC_SAMPLING_RATE*ADC_NUM_OF_CH,
+        .sample_rate = ADC_SAMPLING_RATE,
         .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
         .communication_format = I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_LSB,
         .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
         .intr_alloc_flags = ESP_INTR_FLAG_LEVEL3,
         .dma_buf_count = ADC_DMA_COUNT,
         .dma_buf_len = ADC_BUFFER_SIZE,
-        .use_apll = true};
+        .use_apll = false,
+    };
     //install and start i2s driver
     i2s_driver_install(I2S_NUM, &i2s_config, 0, NULL);
 
@@ -44,18 +46,63 @@ static void adc_i2s_init(void)
         {
             .atten = ADC_ATTEN_DB_11,
             .bits = ADC_WIDTH_BIT_12,
-            .channel = ADC1_CHANNEL_6},
+            .channel = ADC1_CHANNEL_6,
+        },
         {
             .atten = ADC_ATTEN_DB_11,
             .bits = ADC_WIDTH_BIT_12,
             .channel = ADC1_CHANNEL_7
-        },
+        }
     };
 
     i2s_set_adc_mode(ADC_UNIT_1, adc_i2s_pattern, sizeof(adc_i2s_pattern));
+    // Delay necessary or reliable start;
+    vTaskDelay(1000/portTICK_RATE_MS);
+}
+// # ----------------------------------------------
+// # SIGNAL GENERATOR 
+// # ----------------------------------------------
+
+
+void signal_generator_task(void *parameters)
+{
+    static uint8_t table_sin_wave[SIN_WAVE_NUM] = {
+    // Sin wave
+        128,131,134,137,141,144,147,150,153,156,159,162,165,168,171,174,
+        177,180,183,186,188,191,194,196,199,202,204,207,209,212,214,216,218,221,223,225,227,229,231,233,234,236,
+        238,239,241,242,243,245,246,247,248,249,250,251,252,253,253,254,254,255,255,255,255,255,255,
+        255,255,255,255,254,254,253,253,252,251,251,250,249,248,247,245,244,243,241,240,238,
+        237,235,233,232,230,228,226,224,222,220,217,215,213,210,208,205,203,200,198,195,192,190,187,184,181,178,176,
+        173,170,167,164,161,158,155,151,148,145,142,139,136,133,130,126,123,120,117,114,111,108,105,101,
+        98,95,92,89,86,83,80,78,75,72,69,66,64,61,58,56,53,51,48,46,43,41,39,36,34,32,30,28,26,24,23,21,
+        19,18,16,15,13,12,11,9,8,7,6,5,5,4,3,3,2,2,1,1,1,1,1,1,1,1,1,1,2,2,3,3,4,5,6,7,8,9,10,
+        11,13,14,15,17,18,20,22,23,25,27,29,31,33,35,38,40,42,44,47,49,52,54,
+        57,60,62,65,68,70,73,76,79,82,85,88,91,94,97,100,103,106,109,112,115,119,122,125
+    };
+
+    uint16_t i = 0;
+    // Enable DAC CHANNEL 1
+    dac_output_enable(DAC_CHANNEL_1);
+    int64_t start_time = 0;
+    while (true){
+
+        // To not hog the CPU
+        if( (esp_timer_get_time() - start_time >= 65) || start_time == 0){   // Check delta in uS
+            dac_output_voltage(DAC_CHANNEL_1, table_sin_wave[i]);
+            i++;
+            if (i >= SIN_WAVE_NUM) i = 0;
+
+            start_time = esp_timer_get_time();
+        }
+        // Feed watchdog
+        TIMERG0.wdt_wprotect=TIMG_WDT_WKEY_VALUE;
+        TIMERG0.wdt_feed=1;
+        TIMERG0.wdt_wprotect=0;
+
+    }
 }
 
-static void testAdc(void *p)
+void sample_read_task(void *parameters)
 {
     	// Local variables
 	int64_t start_time = 0;        			// Start time tick [us]
@@ -73,12 +120,12 @@ static void testAdc(void *p)
     //read data from I2S bus, in this case, from ADC.
     size_t got;
     // Obtain current time
-    i2s_adc_enable(I2S_NUM);
-    for (int k = 0; k < 2; ++k) {
-        start_time = esp_timer_get_time();
+    //i2s_adc_enable(I2S_NUM);
+    //while (true) {
+    for (int k = 0; k < 1; ++k) {
+        
         esp_err_t err = i2s_read(I2S_NUM, buf, sizeof(buf), &got, portMAX_DELAY);
-        // Obtain current time
-        end_time = esp_timer_get_time();
+
         if (err != ESP_OK)
         {
             printf("i2s_read: %d\n", err);
@@ -87,25 +134,46 @@ static void testAdc(void *p)
         //DBG_ASSERT(got % sizeof(adc_sample_t) == 0);
         got /= sizeof(adc_sample_t);
 
-        uint32_t data_converted = 0;  // Voltage read;
+        int32_t voltage_converted = 0;  // Voltage read;
+        int32_t current_converted = 0;  // Current read;
 
+
+        uint32_t sum_voltage = 0;    // Voltage sum;
+        uint32_t sum_current = 0;    // Current sum;
+        float rms_voltage = 0;
+        float rms_current = 0;
+        start_time = esp_timer_get_time();
         for (size_t i = 0; i < got; i += ADC_NUM_OF_CH)
         {
-            for (size_t j = 0; j < ADC_NUM_OF_CH; ++j)
-            {
 
-                data_converted = esp_adc_cal_raw_to_voltage(ADC_GET_MEASURE(buf[i + j]), &cal);
-                printf("{Data %d} - [CH-%d]%d mV\n", i + j,buf[i + j] >> 12, data_converted);
-            }
-
-            printf("\n");
+            //voltage_converted = (int32_t) ADC_GET_MEASURE(buf[i]) - ADC_SIGNAL_OFFSET;
+            //current_converted = (int32_t) ADC_GET_MEASURE(buf[i+1]) - ADC_SIGNAL_OFFSET;
+            printf("%d, %d, ", buf[i+1]>>12, ADC_GET_MEASURE(buf[i+1]));
+            //printf("[CH-%d] %d\n\n", i+1, buf[i+1]);
+            //voltage_converted = esp_adc_cal_raw_to_voltage(ADC_GET_MEASURE(buf[i]), &cal) - ADC_V_REF / 2;
+            current_converted = esp_adc_cal_raw_to_voltage(ADC_GET_MEASURE(buf[i+1]), &cal);
+            printf("%d, ", current_converted);
+            current_converted = current_converted - ADC_V_REF / 2;
+            printf("%d, ", current_converted);
+            //ESP_LOGI("ADC", "Volt: %d Cur: %d\n", voltage_converted, current_converted);
+            //sum_voltage = (uint32_t) (voltage_converted*voltage_converted);
+            sum_current = (uint32_t) (current_converted*current_converted);
+            printf("%d\n", sum_current);
+            //printf("%d , %d\n", ADC_GET_MEASURE(buf[i]), buf[i] >> 12);
         }
-        
+
+        //rms_voltage = sqrt(sum_voltage / (got/2));
+        rms_current = sqrt(sum_current / (got/2));
+        // Obtain current time
+        end_time = esp_timer_get_time();
         printf("[GOT VARIABLE] = %d\n", got);
         // Calculating time spent
         time_spent = (float)(end_time - start_time) / 1000000;
         // Printing after execution
-        printf("execution time (one block at a time) = %f s\n", time_spent);
+        printf("execution time = %f s\n", time_spent);
+        //printf("Voltage RMS: %f mV\n", rms_voltage);
+        printf("Current RMS: %f mV\n", rms_current);
+
     }
     i2s_adc_disable(I2S_NUM);
     i2s_stop(I2S_NUM);
@@ -118,13 +186,13 @@ void app_main(void)
     // start the main task
     //xTaskCreate(&connect_to_wifi, "con2WIFI", 2048, NULL, 5, NULL);
     //set RGB PINs
-    gpio_pad_select_gpio(LED_R);
-    gpio_pad_select_gpio(LED_G);
-    gpio_pad_select_gpio(LED_B);
+    //gpio_pad_select_gpio(LED_R);
+    //gpio_pad_select_gpio(LED_G);
+    //gpio_pad_select_gpio(LED_B);
     
-    gpio_set_direction(LED_R, GPIO_MODE_OUTPUT);
-    gpio_set_direction(LED_G, GPIO_MODE_OUTPUT);
-    gpio_set_direction(LED_B, GPIO_MODE_OUTPUT);
+    //gpio_set_direction(LED_R, GPIO_MODE_OUTPUT);
+    //gpio_set_direction(LED_G, GPIO_MODE_OUTPUT);
+    //gpio_set_direction(LED_B, GPIO_MODE_OUTPUT);
 
     esp_err_t ret = nvs_flash_init();
 
@@ -135,8 +203,49 @@ void app_main(void)
     }
 
     ESP_ERROR_CHECK(ret);
+    //wifi_init_softap();
 
 
-    wifi_init_softap();
-    //xTaskCreate(testAdc, "testAdc", 1024 * 8, NULL, configMAX_PRIORITIES - 1, NULL);
+    // Local variables
+	BaseType_t task_create_ret;				// Return of task create
+	// Print info to show main task is running
+	ESP_LOGI(TAG_SM, "# Running app_main in core %d", xPortGetCoreID());
+
+    // Log task creation
+	ESP_LOGI(TAG_SM, "# Creating signal_generator task");
+
+	// Create task to write audio to wav file in SD card
+	task_create_ret = xTaskCreatePinnedToCore(
+		signal_generator_task,					// Function executed in the task
+		"SGT",					            // Task name (for debug)
+		4096,								// Stack size in bytes
+		NULL,								// Parameter to pass to the function
+		1,									// Task priority
+		&signal_generator_task_handle,			// Used to pass back a handle by which the created task can be referenced
+		1);									// CPU core ID
+
+    // Check task creation error
+	if (task_create_ret != pdPASS){ ESP_LOGE(TAG_SM, "Error creating signal_generator task"); }
+	// Delay 
+	vTaskDelay(1000 / portTICK_PERIOD_MS);
+    // Log task creation
+	ESP_LOGI(TAG_SM, "# Creating sample_read_task task");
+
+    
+	// Create task to write audio to wav file in SD card
+	task_create_ret = xTaskCreatePinnedToCore(
+		sample_read_task,					// Function executed in the task
+		"SRT",					            // Task name (for debug)
+		4096,								// Stack size in bytes
+		NULL,								// Parameter to pass to the function
+		1,									// Task priority
+		&sample_read_task_handle,			// Used to pass back a handle by which the created task can be referenced
+		0);									// CPU core ID
+
+    // Check task creation error
+	if (task_create_ret != pdPASS){ ESP_LOGE(TAG_SM, "Error creating sample_read task"); }
+
+
+    
+    
 }
--- src/smartmeter.h
+++ src/smartmeter.h
@@ -14,6 +14,7 @@
 #include <stdio.h>                          // C standard library
 #include <stddef.h>                         // C standard library
 #include <stdint.h>                         // C standard library
+#include <math.h>
 
 #include <driver/gpio.h>                    // ESP-IDF library for gpio control
 #include <driver/i2s.h>                     // ESP-IDF library for i2s drive
@@ -33,9 +34,29 @@
 // Application lib files
 #include "wifi.h"                           // Wi-fi functions
 
+// Signal generator library
+#include <driver/dac.h>
+// - Watchdog
+#include "soc/timer_group_reg.h"
+#include "soc/timer_group_struct.h"
+// #-------------------------------------
+// # Global variables 
+// #-------------------------------------
+
+static const char *TAG_SM = "SM";	        // Define log tag
+
+// Task handles
+TaskHandle_t sample_read_task_handle = NULL;	    // sample_read_task handle
+TaskHandle_t signal_generator_task_handle = NULL;	// signal_generator_task handle
 
 // Define uint to adc sample
 typedef uint16_t adc_sample_t;
+
+// Measurements sample task
+void sample_read_task(void *parameters);
+
+// Task to simulate signal
+void signal_generator_task(void *parameters);
 
 
 
